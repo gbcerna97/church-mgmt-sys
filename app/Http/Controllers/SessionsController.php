04188ae798153e3b3b4c5cfec3bedb8bae609921
gbcerna97 <gbcerna1997@gmail.com>
2023-05-23 13:51:25 +0800

    Updated auth controllers, added user_type to User

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SessionsController extends Controller
+{
+    public function create()
+    {
+        return view('session.login-session');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($attributes)) {
+            $user = Auth::user();
+            $token = $user->createToken('personal-access-token')->plainTextToken;
+            return redirect('home')->with(['success' => 'You are logged in.', 'token' => $token]);
+        } else {
+            return back()->withErrors(['email' => 'Email or password invalid.']);
+        }
+    }
+
+    public function destroy()
+    {
+        $user = Auth::user();
+        $user->tokens()->delete();
+        Auth::guard('web')->logout();
+
+        return redirect('/login')->with(['success' => 'You have been logged out.']);
+    }
+}
+
+
